30bcab38578e8d125435f2cfb30b2d7fcd086929
xw <1782332961@qq.com>
2022-06-29 09:25:14 +0800

    完成了登录和注册的开发

---
+++ assets/js/baseAPI.js
@@ -0,0 +1,10 @@
+//每次调用$.get(),$.post()或$.ajax()的时候
+//会先调用ajaxPrefilter这个函数
+//在这个函数中，可以拿到我们给ajax提供的配置对象
+$.ajaxPrefilter(function (option) {
+
+  //在发起真正的ajax请求之前，统一拼接请求的根路径
+  option.url='http://www.liulongbin.top:3007'+option.url
+  console.log(option.url);
+  
+})
